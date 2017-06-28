372de8fdb45574636f6903be119c0777c4304892
System User <pedro.delicado@gamil.com>
2017-06-28 09:53:33 +0200

    finished day 1

---
+++ pedro_delicado/configuration.R
@@ -0,0 +1,7 @@
+#DATA_DIR  <- file.path("..", "data")
+#INSTACART_DATA_DIR <- file.path("..", "instacart_data")
+#FOOTBALL_DATA_DIR <- file.path("..", "football_data")
+
+DATA_DIR  <- file.path("data")
+INSTACART_DATA_DIR <- file.path("instacart_data")
+FOOTBALL_DATA_DIR <- file.path("football_data")
